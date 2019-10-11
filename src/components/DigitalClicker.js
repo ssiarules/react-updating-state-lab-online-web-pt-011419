9458151ecf5b8dbceb7f98159358d51569f34c2e
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-11 04:37:25 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+
+  constructor(){
+    super();
+    timesClicked: 0
+  }
+
+
+  render(){
+    return(
+          <button input={this.timesClicked}>
+
+          </button>
+
+    )
+  }
+}
+export default DigitalClicker
