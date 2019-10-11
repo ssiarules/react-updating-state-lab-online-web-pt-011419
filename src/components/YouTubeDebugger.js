2d0e465b5cd94c5f3077cac2898f2c19fd1fe84f
Johnson Joseph <jhonsonj88@gmail.com>
2019-10-11 05:17:39 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,11 @@
 // Code YouTubeDebugger Component Here
+import React from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0
+    }
+  }
